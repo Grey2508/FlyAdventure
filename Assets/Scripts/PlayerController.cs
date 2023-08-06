070fe16fe0966d1fce983b1f5d1db756fb181916
Grey2508 <grey-_@mail.ru>
2023-08-07 00:01:39 +0500

    Change playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,63 @@
-using System.Linq;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : ManagedObject
 {
     [SerializeField] private float _force = 20f;
-    [SerializeField] private KeyCode[] _controlKeys;
+    [SerializeField] private float _rotationMultiplier = 5f;
 
     private Rigidbody2D _rigidbody;
 
-    private void Start()
+    private bool _isPlaying = true;
+
+    private GameManager _gameManager;
+
+    private Vector2 _startPosition;
+
+    private Transform _transform;
+
+    public Transform Transform => _transform;
+
+    public override void Initialize(GameManager manager)
     {
+        base.Initialize(manager);
+
         _rigidbody = GetComponent<Rigidbody2D>();
+        _gameManager = manager;
+
+        _transform = transform;
+
+        _startPosition = transform.position;
     }
 
     private void Update()
     {
-        if (_controlKeys.Length == 0)
+        if (_isPlaying == false)
             return;
 
-        for (int i = 0; i < _controlKeys.Length; i++)
+        if (Input.GetButton("Action") == true)
         {
-            if (Input.GetKey(_controlKeys[i]))
-            {
-                _rigidbody.AddForce(new Vector2(0, _force * Time.deltaTime), ForceMode2D.Impulse);
-                return;
-            }
+            _rigidbody.AddForce(new Vector2(0, _force * Time.deltaTime), ForceMode2D.Impulse);
         }
+
+        _rigidbody.SetRotation(_rigidbody.velocity.y * _rotationMultiplier);
+    }
+
+    protected override void StartGame()
+    {
+        _transform.position = _startPosition;
+
+        _isPlaying = true;
+        _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    protected override void StopGame()
+    {
+        _isPlaying = false;
+        _rigidbody.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _gameManager.StopGame();
     }
 }
